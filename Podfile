69ca19f0132acbe2290a9f64034b4c66063f2cd6
Aviv Wolf <post.pc.developer@gmail.com>
2014-01-16 18:05:17 +0200

    Added InAppSettingsKit to pods.

--- Podfile
+++ Podfile
@@ -1,3 +1,4 @@
 platform :ios,          '6.0'
 pod 'AFNetworking',     '~> 2.0.3'
+pod 'InAppSettingsKit', '~> 2.0'
 ##pod 'AWSiOSSDK',  		'~> 1.7.1'
--- Pods/Pods-environment.h
+++ Pods/Pods-environment.h
@@ -48,3 +48,9 @@
 #define COCOAPODS_VERSION_MINOR_AFNetworking_UIKit 0
 #define COCOAPODS_VERSION_PATCH_AFNetworking_UIKit 3
 
+// InAppSettingsKit
+#define COCOAPODS_POD_AVAILABLE_InAppSettingsKit
+#define COCOAPODS_VERSION_MAJOR_InAppSettingsKit 2
+#define COCOAPODS_VERSION_MINOR_InAppSettingsKit 0
+#define COCOAPODS_VERSION_PATCH_InAppSettingsKit 0
+
